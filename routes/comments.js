2e090cf5f7af34eb75e4afd3b50cd0e1ae381574
Y user.email you@example.comour Name <c.paoli92@gmail.com>
2018-02-13 13:59:46 +0000

    update all routes

--- routes/comments.js
+++ routes/comments.js
@@ -1,81 +1,88 @@
 var express = require("express");
-var router  = express.Router({mergeParams: true});
+var router = express.Router({ mergeParams: true });
 var Pet = require("../models/pet");
 var Comment = require("../models/comment");
 var middleware = require("../middleware");
 
 // comments
-router.get("/new", middleware.isAuthenticated, function(req, res){
- Pet.findById(req.params.id, function(err, pet){
-        if (err){
+router.get("/new", middleware.isAuthenticated, function(req, res) {
+    Pet.findById(req.params.id, function(err, pet) {
+        if (err) {
             console.log(err);
         }
-        else{
-             res.render("comment", {pet: pet, req: req});
+        else {
+            res.render("comment", { pet: pet });
         }
     });
 });
 
-router.post("/", middleware.isAuthenticated, function (req, res) {
-    Pet.findById(req.params.id, function(err, pet){
-        if (err){
-         console.log(err);
-        } else {
-           Comment.create(req.body.comment, function(err, comment){
-            comment.author.id =  req.user.id;
-            comment.author.username =  req.user.username;
-               if(err){
-                console.log(err);
-               } else {
-                 comment.save();
-                 pet.comments.push(comment._id);
-                 pet.save();
-                 res.redirect('/pets/' + pet._id);
-               }
-            }); 
+router.post("/", middleware.isAuthenticated, function(req, res) {
+    Pet.findById(req.params.id, function(err, pet) {
+        if (err) {
+            console.log(err);
         }
-    });
-});
-
-router.put('/:comm_id', middleware.checkUserComment, function(req, res){
-    Pet.findById(req.params.id, function(err, pet){
-        if (err){
-         console.log(err);
-        } else {
-        	Comment.findByIdAndUpdate(req.params.comm_id, req.body.comment, function(err, comment){
-        	 	if(err){
-        	 	    console.log(err);
-        	 	} else {
-        		   res.redirect('/pets/'+req.params.id);
-        		 }
-        	 });
-        }
-    });
-});
-
-router.get('/:comm_id/edit', middleware.checkUserComment, function (req, res){
-     Pet.findById(req.params.id, function(err, pet){
-        if (err){
-         console.log(err);
-        } else {
-            Comment.findById(req.params.comm_id, function(err, comment){
-            if (err) {
-                console.log(err);
-          } else{
-           res.render("editComment", {comment: comment, pet: pet, req: req}); 
-          }
+        else {
+            Comment.create(req.body.comment, function(err, comment) {
+                comment.author.id = req.user.id;
+                comment.author.username = req.user.username;
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    comment.save();
+                    pet.comments.push(comment._id);
+                    pet.save();
+                    res.redirect('/pets/' + pet._id);
+                }
             });
         }
     });
 });
 
-router.delete('/:comm_id', middleware.checkUserComment, function(req, res){
-	Comment.findByIdAndRemove(req.params.comm_id, function (err) {
-          if (err) {
+router.put('/:comm_id', middleware.checkUserComment, function(req, res) {
+    Pet.findById(req.params.id, function(err, pet) {
+        if (err) {
             console.log(err);
-          } else{
-           res.redirect("/pets/"+ req.params.id);
-          }
+        }
+        else {
+            Comment.findByIdAndUpdate(req.params.comm_id, req.body.comment, function(err, comment) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    res.redirect('/pets/' + req.params.id);
+                }
+            });
+        }
+    });
+});
+
+router.get('/:comm_id/edit', middleware.checkUserComment, function(req, res) {
+    Pet.findById(req.params.id, function(err, pet) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            Comment.findById(req.params.comm_id, function(err, comment) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    res.render("editComment", { comment: comment, pet: pet });
+                }
+            });
+        }
+    });
+});
+
+router.delete('/:comm_id', middleware.checkUserComment, function(req, res) {
+    Comment.findByIdAndRemove(req.params.comm_id, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.redirect("/pets/" + req.params.id);
+        }
     });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,51 +1,53 @@
 var express = require("express");
-var router  = express.Router();
+var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
 
 
 router.get("/", function(req, res) {
-    res.render("landing", {req: req});
+    res.render("landing");
 });
 
 router.get('/auth/facebook', passport.authenticate('facebook'));
 
 router.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { successRedirect: '/pets',
-                                      failureRedirect: '/login' }));
+    passport.authenticate('facebook', {
+        successRedirect: '/pets',
+        failureRedirect: '/login'
+    }));
 
 // login - register
 
 router.get('/register', function(req, res) {
-  res.render('register',  {req: req});
+    res.render('register');
 });
 
 router.post('/register', function(req, res) {
-    User.register(new User({ username : req.body.username }), req.body.password, function(err, user) {
+    User.register(new User({ username: req.body.username }), req.body.password, function(err, user) {
         if (err) {
             req.flash("error", err.message);
-            return res.render('register', {req: req});
+            return res.render('register');
         }
-    
-        passport.authenticate('local')(req, res, function () {
-             req.flash("success", "Successfully Signed Up! Nice to meet you " + req.body.username);
-             res.redirect('/pets');
+
+        passport.authenticate('local')(req, res, function() {
+            req.flash("success", "Successfully Signed Up! Nice to meet you " + req.body.username);
+            res.redirect('/pets');
         });
     });
 });
 
 router.get('/login', function(req, res) {
-  res.render('login',  {req: req});
+    res.render('login');
 });
 
-router.post('/login', passport.authenticate('local', 
-{ failureRedirect: '/login', failureFlash: true}), function(req, res) { res.redirect('/pets');
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), function(req, res) {
+    res.redirect('/pets');
 });
 
 router.get('/logout', function(req, res) {
-  req.logout();
-  req.flash("success", "LOGGED YOU OUT!");
-  res.redirect('/pets');
+    req.logout();
+    req.flash("success", "LOGGED YOU OUT!");
+    res.redirect('/pets');
 });
 
 module.exports = router;
